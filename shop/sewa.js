e92e049c6217f35848537a576a39c7708582081e
NezzyXD <nezzyxbotz@gmail.com>
2022-05-10 16:11:58 +0700

    Alphabot

---
+++ shop/sewa.js
@@ -0,0 +1,21 @@
+const pc_sewa = () => {
+return 
+
+`「 *PRICE LIST* 」
+							
+_*LIST SEWA BOT BY FZYY*_
+
+╭─────────    
+│❏ > 𝚂𝙴𝚆𝙰 𝙱𝙾𝚃 1 𝙷𝙰𝚁𝙸 : 2.000
+│❏ > 𝚂𝙴𝚆𝙰 𝙱𝙾𝚃 1 𝙼𝙸𝙽𝙶𝙶𝚄 : 4.000
+│❏ > 𝚂𝙴𝚆𝙰 𝙱𝙾𝚃 1 𝙱𝚄𝙻𝙰𝙽 : 6.000
+│❏ > 𝚂𝙴𝚆𝙰 𝙿𝙴𝚁𝙼𝙰𝙽𝙴𝙽 : 8.000
+
+*BOT  ON 24 JAM*
+*BUKAN BOT WIBUSOFT*
+PAY
+GOPAY
+QRIS ALL PAY+1k `
+	}
+
+exports.pc_sewa = pc_sewa
